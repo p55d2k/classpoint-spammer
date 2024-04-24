57794c4b9945181c18df04a5bef4c9f78a055f84
p55d2k <kai@xcidic.com>
2024-04-24 16:45:49 +0800

    v1.0.0 simplified into 1 file

--- main.py
+++ main.py
@@ -3,7 +3,22 @@ import sys
 import time
 import requests
 
-from src.args import *
+def print_version(extra=""):
+    print("classpoint-spammer v1.0.0" + extra)
+
+def parse_args(args):
+    if args[0] == "-h":
+        print("Usage: python3 main.py [OPTIONS]\n\nOptions:\n  -h, --help\t\tShow this help message and exit\n  -o, --output\t\tOutput file name and exit\n  -v, --version\t\tShow program's version number and exit\n\nIf no options are provided, the program will run with default settings.")
+        sys.exit(0)
+    elif args[0] == "-o" or args[0] == "--output":
+        print("Output file name: /links/links_{current_datetime}.txt")
+        sys.exit(0)
+    elif args[0] == "-v" or args[0] == "--version":
+        print_version()
+        sys.exit(0)
+    else:
+        print("Invalid option. Use -h or --help for help.")
+        sys.exit(1)
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 starttime = time.strftime("%d-%m-%y_%H:%M:%S")
